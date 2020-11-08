cea3715c1b1599b6f70da06dfae5cb0eec83e9d0
haiyugao <1923831552@qq.com>
2020-11-08 22:07:32 +0800

    Add haiyugao

---
+++ app/Models/JSONModel.php
@@ -0,0 +1,67 @@
+<?php
+namespace App\Models\Simple;
+
+use App\Models\Simple\JSONModel;
+
+class Places extends JSONModel
+{
+    protected $origin = WRITEPATH . 'Data/placesData.json';
+    protected $keyField = 'id';
+    protected $validationRules = [];
+}
+
+/**
+ * SimpleModel persisted as JSON document
+ *  
+ */
+class JSONModel extends SimpleModel
+{
+
+	/**
+	 * Constructor.
+	 * @param string $origin Filename of the CSV file
+	 * @param string $keyField  Name of the primary key field
+	 * @param string $entity	Entity name meaningful to the persistence
+	 */
+	function __construct($origin = null, $keyField = 'id', $entity = null)
+	{
+		parent::__construct($origin, $keyField, $entity);
+
+		// and populate the collection
+		$this->load();
+	}
+
+	/**
+	 * Load the collection state from a JSON document
+	 */
+	protected function load()
+	{
+
+		if (file_exists(realpath($this->origin)))
+		{
+
+			$contents = file_get_contents($this->origin);
+			$this->data = json_decode($contents);
+
+			$record = (array) $this->data[0];
+			$this->fields = [];
+			foreach ($record as $key => $value)
+				$this->fields[] = $key;
+		}
+
+		// --------------------
+		// rebuild the keys table
+		$this->reindex();
+	}
+
+	/**
+	 * Store the collection state as a JSON document
+	 */
+	protected function store()
+	{
+
+		$output = json_encode($this->data);
+		file_put_contents($this->origin, $output);
+	}
+
+}
